93f0c45ee44151baba33a335315a35759d379247
ZiF1R <do-alex03@mail.ru>
2022-01-26 17:28:13 +0300

    Add tests for tabulated functions

--- DynamicProgrammingCourse/tabulation.test.js
+++ DynamicProgrammingCourse/tabulation.test.js
@@ -4,6 +4,11 @@ let {
   fib,
   gridTraveler,
   canSum,
+  howSum,
+  bestSum,
+  canConstruct,
+  countConstruct,
+  allConstruct
 } = Tabulation;
 
 describe("fib", () => {
@@ -41,5 +46,63 @@ describe("canSum", () => {
   });
   test("should return false", () => {
     expect(canSum(300, [7, 14])).toBeFalsy();
+  });
+});
+
+describe("howSum", () => {
+  test("basic tests", () => {
+    expect(howSum(7, [5, 3, 4, 7])).toStrictEqual([4, 3]);
+    expect(howSum(8, [2, 3, 5, 4])).toStrictEqual([2, 2, 2, 2]);
+  });
+});
+
+describe("bestSum", () => {
+  test("basic tests", () => {
+    expect(bestSum(7, [5, 3, 4, 7])).toStrictEqual([7]);
+    expect(bestSum(8, [2, 3, 5])).toStrictEqual([5, 3]);
+    expect(bestSum(8, [1, 4, 5])).toStrictEqual([4, 4]);
+    expect(bestSum(2, [5, 3, 4, 7])).toBeNull();
+    expect(bestSum(100, [1, 2, 5, 25])).toStrictEqual([25, 25, 25, 25]);
+  });
+});
+
+describe("canConstruct", () => {
+  test("should return true", () => {
+    expect(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])).toBeTruthy();
+    expect(canConstruct("", ["cat", "dog", "mouse"])).toBeTruthy();
+    expect(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])).toBeTruthy();
+  });
+  test("should return false", () => {
+    expect(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])).toBeFalsy();
+    expect(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"])).toBeFalsy();
+  });
+});
+
+describe("countConstruct", () => {
+  test("basic tests", () => {
+    expect(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"])).toBe(2);
+    expect(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])).toBe(1);
+    expect(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])).toBe(0);
+    expect(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])).toBe(4);
+    expect(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"])).toBe(0);
+  });
+});
+
+describe("allConstruct", () => {
+  test("should return all combinations", () => {
+    expect(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"])).toEqual([
+      ["purp", "le"],
+      ["p", "ur", "p", "le"]
+    ]);
+    expect(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"])).toEqual([
+      ["ab", "cd", "ef"],
+      ["ab", "c", "def"],
+      ["abc", "def"],
+      ["abcd", "ef"]
+    ]);
+  });
+  test("should return an empty array", () => {
+    expect(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])).toEqual([]);
+    expect(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"])).toEqual([]);
   });
 });
